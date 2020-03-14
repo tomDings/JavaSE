6b5bff979035d05ae662cfd9770c4c4d072a5186
dxf <237881603@qq.com>
2020-03-14 16:22:59 +0800

    demo：hello world

---
+++ src/main/java/day_01/HelloWorld.java
@@ -0,0 +1,12 @@
+package day_01;
+
+/**
+ * 程序员入门之Hello World
+ * @author  DXF 237881603@qq.com
+ * @update 2020-03-14
+ * */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
